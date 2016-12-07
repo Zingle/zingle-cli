12ccbdffdf63177a2caaceb81e678dcf84a2176b
Richard Remer <reanjr@gmail.com>
2016-12-07 08:46:32 -0800

    stubbed tests

---
+++ test/test-zingle-cli.js
@@ -0,0 +1,11 @@
+const expect = require("expect.js");
+const CLI = require("..");
+const Interface = CLI.Interface;
+const State = CLI.State;
+
+describe("State", () => {
+    
+});
+
+describe("Interface", () => {
+});
